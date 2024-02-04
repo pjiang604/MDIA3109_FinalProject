004ca73aba6014a3e34cad42a5b378c40ea2dd10
Glenda <gcheung34@my.bcit.ca>
2024-02-03 19:21:20 -0800

    smaller playlist buttons created and folder created for music/art tab

--- src/pages/browse.tsx
+++ src/pages/browse.tsx
@@ -1,14 +1,13 @@
 import HeaderNav from "@/components/navigation/HeaderNav";
 import Nav from "@/components/navigation/NavBar";
-
+import SmallPlaylist from "@/components/buttons/SmallPlaylist"
 
 export default function Browse() {
   return (
     <main className={``} >
       <HeaderNav text="Browse" type="simple-backBtn" />
       <div id="mainContainer" className={`flex flex-col`}>
-
-
+        <SmallPlaylist />
       </div>
       <Nav type="browse" />
     </main>
