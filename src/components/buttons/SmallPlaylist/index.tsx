0e23f00a1cb4502d6b3b6cc307702d8b10dbbfd5
Glenda <gcheung34@my.bcit.ca>
2024-02-04 19:25:14 -0800

    Carousel and tab components created but not complete

--- src/components/buttons/SmallPlaylist/index.tsx
+++ src/components/buttons/SmallPlaylist/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function SmallPlaylist({
 }) {
     return (
-        <div className={`relative w-1/2 h-autorounded-md`}>
+        <div className={`relative w-1/2 h-auto rounded-md`}>
             <div className={`relative flex justify-center items-center`}>
                 <h2 className={`z-10 absolute text-[#f8fafc] font-bold text-xl`}>Kitsilano</h2>
                 <Image
