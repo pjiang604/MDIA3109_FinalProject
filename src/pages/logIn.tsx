6619dbd1ab40d1ecb404ad03da93458680c99fb6
Andrea <113224170+Andreawz3@users.noreply.github.com>
2024-02-07 10:47:12 -0800

    Revert "Revert "add logo, loading dots, and overlay information in index-landing page…""

--- src/pages/logIn.tsx
+++ src/pages/logIn.tsx
@@ -4,7 +4,8 @@ export default function LogIn() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24`} >
       <div id="mainContainer" className={`flex flex-col`}>
-
+        <p>this is the login screen</p>
+        <Link href='/home'><b>Go to home</b></Link>
       </div>
     </main>
   )
