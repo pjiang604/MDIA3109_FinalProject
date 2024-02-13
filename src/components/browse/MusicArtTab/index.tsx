b7eede3bd7aae127935b2fe967cba97dc307bba2
Glenda <gcheung34@my.bcit.ca>
2024-02-12 21:44:35 -0800

    styling tabs panel

--- src/components/browse/MusicArtTab/index.tsx
+++ src/components/browse/MusicArtTab/index.tsx
@@ -1,20 +1,20 @@
 import SmallPlaylist from '@/components/buttons/SmallPlaylist';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import styles from './MusicArtTab.module.css'
 
 export default function MusicArtTab({ //playlists
 }) {
     return (
         <Tabs>
-            <TabList>
-                <Tab>Music</Tab>
-                <Tab>Art</Tab>
+            <TabList className={styles.tabList}>
+                <Tab className={styles.tab} selectedClassName={styles.selectedTab}>Music</Tab>
+                <Tab className={styles.tab} selectedClassName={styles.selectedTab}>Art</Tab>
             </TabList>
-
-            <TabPanel>
+            <TabPanel className={styles.tabPanel}>
                 <SmallPlaylist />
             </TabPanel>
-            <TabPanel>
+            <TabPanel className={styles.tabPanel}>
             <SmallPlaylist />
             <SmallPlaylist />
             </TabPanel>
