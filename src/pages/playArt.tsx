1fd9424a56836b31c12f48a367c10ce4d41656aa
pjiang604 <patriciaejiang@gmail.com>
2024-02-23 16:39:14 -0800

    fixed data mapping update error in browse page

--- src/pages/playArt.tsx
+++ src/pages/playArt.tsx
@@ -4,6 +4,14 @@ import Image from "next/image";
 import Carousel from 'nuka-carousel';
 import styles from "@/styles/PlayArt.module.css"
 import Head from 'next/head'
+import { useRouter } from "next/router";
+import { authorize } from "./api/authorize";
+import SpotifyPlayer from 'react-spotify-web-playback';
+import { getPlaylist } from "./api/getMusic";
+
+// Components
+import Nav from "@/components/navigation/NavBar"
+import HeaderNav from "@/components/navigation/HeaderNav";
 
 enum ArtUntitled {
   Untitled = "Untitled", // Untitled
@@ -12,14 +20,17 @@ enum ArtUntitled {
   Fountain = "Untitled (fountain)", // Untitled (fountain)
 }
 
-// Components
-import Nav from "@/components/navigation/NavBar"
-import HeaderNav from "@/components/navigation/HeaderNav";
-
 export default function PlayArt() {
 
   const [data, setData] = useState<PublicArt[]>([]);
   const [headerTitle, setHeadertitle] = useState<string>(`Captain George Vancouver`)
+
+  const router = useRouter();
+  const [playlistData, setPlaylistData] = useState<ArtistTopTracks>()
+  const playlist_id = "spotify:playlist:2fuBkF1RV4sicOMYFDCRQh"
+  const [accessToken, setAccessToken] = useState("");
+  const song = `${router.query.songUri}`;
+  const track_num = Number(router.query.track_num);
 
   const url = "https://opendata.vancouver.ca/api/explore/v2.1/catalog/datasets/public-art/records?limit=23"
 
@@ -30,14 +41,33 @@ export default function PlayArt() {
       setData(response.data.results);
     }
 
+    const fetchPlaylist = async () => {
+      try {
+        const data = await getPlaylist(playlist_id);
+        setPlaylistData(data);
+        console.log("playlist data: ", data);
+      } catch (error) {
+        console.error("Error fetching playlist:", error);
+      }
+    };
+    fetchPlaylist();
     getData()
 
       .catch(console.error);
   }, []);
 
-const changeSlideHeader = (slideIndex: number) => {
-setHeadertitle(data[slideIndex].title_of_work)
-}
+  const changeSlideHeader = (slideIndex: number) => {
+    setHeadertitle(data[slideIndex].title_of_work)
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setAccessToken(`${localStorage.getItem("access_token")}`)
+
+    } else {
+      console.log("play music page: local storage undefined")
+    }
+  }, [authorize])
 
   return (
     <main className={``} >
@@ -47,7 +77,7 @@ setHeadertitle(data[slideIndex].title_of_work)
       <HeaderNav
         type="simple-backBtn"
         text={headerTitle} />
-      <div>
+      <div className={`flex flex-col`}>
         <Carousel style={{ marginBottom: "100px" }}
           // renderCenterLeftControls={() => null}
           // renderCenterRightControls={() => null}
@@ -87,7 +117,43 @@ setHeadertitle(data[slideIndex].title_of_work)
             })
           }
         </Carousel>
+        <div className={`mb-12`}>
+          {
+            accessToken ?
+              <SpotifyPlayer
+                token={accessToken}
+                uris={playlist_id}
+                play={true}
+                hideAttribution={true}
+                offset={track_num}
+                hideCoverArt={true}
+                styles={{
+                  bgColor: "var(--black)",
+                  color: "var(--white)",
+                  trackArtistColor: "var(--white)",
+                  trackNameColor: "var(--white)",
+                }}
+
+              />
+              :
+              <>
+                {
+                  !accessToken ?
+                    <p>Please log in to play music!</p>
+                    :
+                    <>
+                      {
+                        !song &&
+                        <></>
+                      }
+                    </>
+                }
+              </>
+          }
+        </div>
+
       </div>
+
 
       <Nav type="art" />
     </main>
