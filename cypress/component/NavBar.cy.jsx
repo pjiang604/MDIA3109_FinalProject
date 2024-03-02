ce83ff9d3095a2ea88d7d68efb6158fa31d88a53
Glenda <gcheung34@my.bcit.ca>
2024-03-02 14:17:55 -0800

    removed cypress component testing files; not needed

--- cypress/component/NavBar.cy.jsx
+++
@@ -1,8 +0,0 @@
-import NavBar from '../../src/components/navigation/NavBar';
-
-describe('NavBar.cy.jsx', () => {
-  it('should contain Home', () => {
-      cy.mount(<NavBar />)
-      cy.contains('Home');
-  })
-})
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack'); 
-
-module.exports = {
-  module: {
-    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
-  },
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
-};
