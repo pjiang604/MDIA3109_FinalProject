0221047b639c7da86285f4a055e489aa8d02bc8a
Glenda <gcheung34@my.bcit.ca>
2024-03-02 14:53:51 -0800

    three cypress tests done for nav bars

--- cypress/e2e/1-amplify-tests/tests.cy.ts
+++ cypress/e2e/1-amplify-tests/tests.cy.ts
@@ -19,15 +19,36 @@ describe('Log In Page', () => {
     })
 })
 
-describe('Loading Page', () => {
-    it('index (loading) page has loading dots', () => {
-        cy.visit('http://localhost:3000');
-        cy.get('.loading_dots').should('be.visible');
-    })
-})
+// describe('Loading Page', () => {
+//     it('index (loading) page has loading dots', () => {
+//         cy.visit('http://localhost:3000');
+//         cy.get('.loading_dots').should('be.visible');
+//     })
+// })
 
 describe('Nav bar', () => {
     it('nav bar has icon images', () => {
-        cy.visit('http://localhost:3000/home')
+        cy.visit('http://localhost:3000/home');
+        cy.get('img').should('be.visible');
+    })
+
+    it('clicking on nav bar links/icons brings you to the respective page', () => {
+        cy.visit('http://localhost:3000/home');
+        cy.get('a[href*="/home"]').click();
+        cy.get('h2').should('be.visible');
+        cy.get('a[href*="/browse"]').click();
+        cy.get('h3').should('contain', 'Past Searches');
+        cy.get('a[href*="/allNeighbourhood"]').click();
+        cy.get('img').should('be.visible');
+        cy.get('a[href*="/playArt"]').click();
+        cy.get('img').should('be.visible');
+    })
+})
+
+
+describe('header nav', () => {
+    it('header nav has h1', () => {
+        cy.visit('http://localhost:3000/home');
+        cy.get('h1').should('be.visible');
     })
 })
