24d1a7abfed7f902c29b7de8b8ee5cb52c28c4ed
Glenda <gcheung34@my.bcit.ca>
2024-03-01 20:43:40 -0800

    configuring cypress tests

--- cypress/e2e/1-amplify-tests/tests.cy.ts
+++ cypress/e2e/1-amplify-tests/tests.cy.ts
@@ -1,5 +1,5 @@
 describe('Home Page', () => {
-    it('have an h2 title on page', () => {
+    it('show h2 title on page', () => {
         cy.visit('http://localhost:3000/home');
         cy.get('h2').should('be.visible');
     })
@@ -7,14 +7,27 @@ describe('Home Page', () => {
 })
 
 describe('Log In Page', () => {
-it('log in page has an input box for both email and password', () => {
-    cy.visit('http://localhost:3000/logIn');
-    cy.get('input[type=email]').should('be.visible');
-    cy.get('input[type=password').should('be.visible');
+    it('log in page has an input box for both email and password', () => {
+        cy.visit('http://localhost:3000/logIn');
+        cy.get('input[type=email]').should('be.visible');
+        cy.get('input[type=password').should('be.visible');
+    })
+
+    it('log in page has a sign in button', () => {
+        cy.visit('http://localhost:3000/logIn');
+        cy.get('button').should('contains.text', 'LOGIN');
+    })
 })
 
-it('log in page has a sign in button', () => {
-    cy.visit('http://localhost:3000/logIn');
-    cy.get('button').should('contains.text', 'LOGIN');
+describe('Loading Page', () => {
+    it('index (loading) page has loading dots', () => {
+        cy.visit('http://localhost:3000');
+        cy.get('.loading_dots').should('be.visible');
+    })
 })
+
+describe('Nav bar', () => {
+    it('nav bar has icon images', () => {
+        cy.visit('http://localhost:3000/home')
+    })
 })
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); 
+
+module.exports = {
+  module: {
+    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+};
