40580b5be9f74eba93d80f441ec33d8c2bcead02
HediehKh <113653779+HediehKh@users.noreply.github.com>
2024-03-04 11:49:13 -0800

    tablet sizing for AllNeighbours

--- src/components/buttons/Playlist/index.tsx
+++ src/components/buttons/Playlist/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import { useState } from "react";
+import styles from './Playlist.module.css'
 
 export default function Playlist({
     name,
@@ -16,20 +17,16 @@ export default function Playlist({
                     <h2 className={`z-10 absolute text-[#f8fafc] font-bold text-xl`}>{name}</h2>
 
                     <Image
+                        className={styles.carouselImage}
                         src={image}
                         width={140}
                         height={140}
                         alt="neighbourhood"
-                        className={`object-fit blur-xs rounded-md z-0`} 
-                        style={{
-                            width: '140px',
-                            height: '140px'
-                        }}
                         />
 
                 </div>
                 <div className={`flex text-center justify-center`}>
-                    <p className={`font-medium uppercase text-sm bg-zinc-400`}>20 songs · 57 mins</p>
+                    <p className={`font-medium uppercase text-sm bg-zinc-400 mt-5`}>20 songs · 57 mins</p>
                 </div>
             </div>
         </Link>
